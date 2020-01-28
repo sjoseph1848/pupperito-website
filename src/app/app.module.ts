cd4aabe4f71e8912d16bee2301b6754e1c773871
sjoseph1848 <sjoseph1848@gmail.com>
2020-01-27 21:52:31 -0600

    added the header and about sec

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './home/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
